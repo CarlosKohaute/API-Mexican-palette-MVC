2409c30642a25bfe28a9414f4dbd2abd51b402ef
Carlos Kohaute <97993704+CarlosKohaute@users.noreply.github.com>
2022-04-17 17:57:12 -0300

    commit index

--- index.js
+++ index.js
@@ -11,23 +11,30 @@ const palettes = [
     id: 1,
     flavor: 'acai com leite condensado',
     description: 'Paleta de acai com leite condensado',
-    photo: '/assets/images/acai-com-leite-condensado.png',
+    photo: './assets/images/acai-com-leite-condensado.png',
     price: 10.0,
   },
   {
     id: 2,
     flavor: 'Banana com Nutella',
     description: 'Paleta de banana com nutella',
-    photo: '/assets/images/banana-com-nutella.png',
+    photo: './assets/images/banana-com-nutella.png',
     price: 10.0,
   },
   {
     id: 3,
     flavor: 'Chocolate Belga com Brigadeiro',
     description: 'Paleta de chocolate belga com brigadeiro',
-    photo: '/assets/images/chocolate-belga-com-brigadeiro.png',
+    photo: './assets/images/chocolate-belga-com-brigadeiro.png',
     price: 10.0,
   },
+  {
+    id: 4,
+    flavor: 'Chocolate Belga',
+    description: 'Paleta de chocolate belga',
+    photo: './assets/images/chocolate-belga.png',
+    price: 10.0,
+  }
 ];
 
 app.get('/palettes/find-palettes', (req, res) => {
